5342f3a6f5f064b5854f6b73dd1f687bbf805aab
Cocker <atormasturbator@o2.pl>
2026-02-12 21:58:51 +0100

    Fixing Firefox mobile crashes (thank you pixel ~9)

--- vite.config.ts
+++ vite.config.ts
@@ -27,12 +27,11 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
-          },
-        }
-      ]
-    }
-})
-],
+          }
+        ]
+      }
+    })
+  ],
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
